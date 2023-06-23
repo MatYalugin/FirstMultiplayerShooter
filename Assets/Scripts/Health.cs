78dbe310c6c37cdb57d9ed8edfceb1e7ea854a23
MAT-ZENBOOK\unity <matyaluginx@gmail.com>
2023-06-23 13:17:31 +0300

    Added a short player immortality time when the player first connected and when he/she was killed.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using SWNetwork;
 
-public class Health : MonoBehaviour {
-
+public class Health : MonoBehaviour
+{
     public delegate void DeathDelegate();
     public event DeathDelegate OnDeath;
 
@@ -18,7 +18,21 @@ public class Health : MonoBehaviour {
     [SerializeField] Text abovePlayerText;
     [SerializeField] Text uiText;
 
-    private void Start() {
+    public Material auraMat;
+    private Material previousPlayerMat;
+    public bool isImmortal;
+
+    public GameObject rifleHand1;
+    public GameObject rifleHand2;
+    public GameObject pistolHand;
+    public GameObject knifeHand;
+    public GameObject playerMesh;
+
+    private void Start()
+    {
+        previousPlayerMat = playerMesh.GetComponent<MeshRenderer>().material;
+        isImmortal = true;
+        Invoke("MakeNotImmortal", 3f);
         if (networkID.IsMine)
         {
             uiText = GameObject.FindGameObjectWithTag("HealthText").GetComponent<Text>();
@@ -27,44 +41,90 @@ public class Health : MonoBehaviour {
                 uiText.text = currentHealth.ToString();
         }
 
-
         if (syncPropertyAgent.GetPropertyWithName(healthKey).version == 0)
             syncPropertyAgent.Modify(healthKey, healthMax);
     }
 
-    public void Damage(float value) {
-        OnHealthChanged(-value);
+    private void Update()
+    {
+        if (isImmortal)
+        {
+            float currentHealth = healthMax;
+            if (uiText != null)
+                uiText.text = currentHealth.ToString();
+
+            rifleHand1.GetComponent<MeshRenderer>().material = auraMat;
+            rifleHand2.GetComponent<MeshRenderer>().material = auraMat;
+            pistolHand.GetComponent<MeshRenderer>().material = auraMat;
+            knifeHand.GetComponent<MeshRenderer>().material = auraMat;
+            playerMesh.GetComponent<MeshRenderer>().material = auraMat;
+        }
+        else
+        {
+            rifleHand1.GetComponent<MeshRenderer>().material = previousPlayerMat;
+            rifleHand2.GetComponent<MeshRenderer>().material = previousPlayerMat;
+            pistolHand.GetComponent<MeshRenderer>().material = previousPlayerMat;
+            knifeHand.GetComponent<MeshRenderer>().material = previousPlayerMat;
+            playerMesh.GetComponent<MeshRenderer>().material = previousPlayerMat;
+        }
     }
 
-    public void Heal(float value) {
+    public void Damage(float value)
+    {
+        if (!isImmortal)
+        {
+            OnHealthChanged(-value);
+        }
+    }
+
+    public void Heal(float value)
+    {
         OnHealthChanged(value);
     }
 
-    public void Kill() {
+    public void Kill()
+    {
         OnHealthChanged(-healthMax);
     }
 
-    public void Restore() {
+    public void Restore()
+    {
         OnHealthChanged(healthMax);
     }
 
-    private void OnHealthChanged(float healthDelta) {
+    private void OnHealthChanged(float healthDelta)
+    {
         float currentHealth = syncPropertyAgent.GetPropertyWithName(healthKey).GetFloatValue();
         currentHealth = Mathf.Clamp(currentHealth + healthDelta, 0, healthMax);
 
         syncPropertyAgent.Modify(healthKey, currentHealth);
     }
 
-    public void OnHealthChanged() {
+    public void OnHealthChanged()
+    {
         float currentHealth = syncPropertyAgent.GetPropertyWithName(healthKey).GetFloatValue();
 
         if (currentHealth == 0 && OnDeath != null && networkID.IsMine)
             OnDeath.Invoke();
+
+        if (currentHealth == 0)
+        {
+            Invoke("MakeImmortalWithDelay", 0.1f);
+            Invoke("MakeNotImmortal", 2f);
+        }
 
         if (abovePlayerText != null)
             abovePlayerText.text = currentHealth.ToString();
 
         if (uiText != null)
             uiText.text = currentHealth.ToString();
+    }
+    public void MakeImmortalWithDelay()
+    {
+        isImmortal = true;
+    }
+    public void MakeNotImmortal()
+    {
+        isImmortal = false;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,8 +3,8 @@ using UnityEngine.UI;
 using SWNetwork;
 
 [RequireComponent(typeof(NetworkID))]
-public class Player : MonoBehaviour {
-
+public class Player : MonoBehaviour
+{
     public Health health;
 
     private Text tipText;
@@ -17,62 +17,70 @@ public class Player : MonoBehaviour {
     bool sprintOn;
 
     public AudioListener audioListener;
-
     public GameObject playerCameraGO;
 
-
-    private void Start() {
+    private void Start()
+    {
         rb = GetComponent<Rigidbody>();
         networkID = GetComponent<NetworkID>();
         speed = defaultSpeed;
 
         playerCameraGO.SetActive(true);
 
-        // changed
-        if (networkID.IsMine) {
+        if (networkID.IsMine)
+        {
             tipText = GameObject.FindGameObjectWithTag("TipText").GetComponent<Text>();
 
             audioListener.enabled = true;
 
             playerCameraGO.GetComponent<FirstPersonLook>().enabled = true;
-
-        } else {
+        }
+        else
+        {
             audioListener.enabled = false;
 
             playerCameraGO.GetComponent<FirstPersonLook>().enabled = false;
             playerCameraGO.GetComponent<Camera>().enabled = false;
         }
 
-        if (health != null) {
+        if (health != null)
+        {
             health.OnDeath += OnPlayerDeath;
         }
     }
 
-    void Update() {
-        if (networkID.IsMine) {
+    void Update()
+    {
+        if (networkID.IsMine)
+        {
             Sprint();
             Move();
             speed = sprintOn ? sprintSpeed : defaultSpeed;
         }
     }
 
-    private void OnPlayerDeath() {
+    public void OnPlayerDeath()
+    {
         health.Restore();
         gameObject.transform.position = Respawn.FindSafestFor(this);
     }
 
-    public void Move() {
+    public void Move()
+    {
         float H = Input.GetAxis("Horizontal");
         float V = Input.GetAxis("Vertical");
         Vector3 Move = transform.right * H * speed + transform.forward * V * speed;
         rb.velocity = new Vector3(Move.x, rb.velocity.y, Move.z);
     }
 
-    public void Sprint() {
-        if (Input.GetKey(KeyCode.LeftShift)) {
+    public void Sprint()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
             sprintOn = true;
         }
-        if (Input.GetKeyUp(KeyCode.LeftShift)) {
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
             sprintOn = false;
         }
     }
